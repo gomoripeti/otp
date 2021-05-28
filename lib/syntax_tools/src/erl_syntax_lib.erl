232b42fc334d0c6721a9319d3e31921e8122a6df
Péter Gömöri <gomoripeti@gmail.com>
2021-05-28 19:55:14 +0200

    WIP Support named funs in erl_syntax_lib:annotate_varbinds

--- lib/syntax_tools/src/erl_syntax_lib.erl
+++ lib/syntax_tools/src/erl_syntax_lib.erl
@@ -538,6 +538,8 @@ vann(Tree, Env) ->
             vann_function(Tree, Env);
         fun_expr ->
             vann_fun_expr(Tree, Env);
+        named_fun_expr ->
+            vann_named_fun_expr(Tree, Env);
         list_comp ->
             vann_list_comp(Tree, Env);
         binary_comp ->
@@ -580,6 +582,14 @@ vann_fun_expr(Tree, Env) ->
     Cs = erl_syntax:fun_expr_clauses(Tree),
     {Cs1, {_, Free}} = vann_clauses(Cs, Env),
     Tree1 = rewrite(Tree, erl_syntax:fun_expr(Cs1)),
+    Bound = [],
+    {ann_bindings(Tree1, Env, Bound, Free), Bound, Free}.
+
+vann_named_fun_expr(Tree, Env) ->
+    N1 = erl_syntax:named_fun_expr_name(Tree),
+    Cs = erl_syntax:named_fun_expr_clauses(Tree),
+    {Cs1, {_, Free}} = vann_clauses(Cs, Env),
+    Tree1 = rewrite(Tree, erl_syntax:named_fun_expr(N1, Cs1)),
     Bound = [],
     {ann_bindings(Tree1, Env, Bound, Free), Bound, Free}.
 
